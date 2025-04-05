6b4102e4ad3c9b931dbfc155e448142b226dcb56
islam543 <i.alabsi0906@gmail.com>
2025-04-05 12:48:36 +0300

    .

--- Website/adminPage.js
+++ Website/adminPage.js
@@ -176,6 +176,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
+
     document.getElementById("logoutButton").addEventListener("click", () => {
         sessionStorage.removeItem('adminId');
         setTimeout(() => {
